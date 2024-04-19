6f0920a12a5db4ff1f010073b7051b3b86653874
rpossardt <rune.possardt@st.ovgu.de>
2024-04-19 17:54:55 +0200

    feat: ros2 setup + assignment 1

--- src/watchdog/watchdog/watchdog.py
+++ src/watchdog/watchdog/watchdog.py
@@ -8,19 +8,36 @@ class WatchdogNode(Node):
 
     def __init__(self):
         super().__init__('watchdog')
+        self.turtle_can_turn_ = 1
+        self.turtle_can_move_ = 1
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.create_subscription(Twist, 'input_cmd', self.cmd_callback, 10)
         self.create_subscription(String, 'controller_cmd', self.controller_callback, 10)
         self.get_logger().info('Watchdog node started')
 
-    def cmd_callback(self, msg):
+    def cmd_callback(self, msg: Twist):
         # this makes the turle go backwards
         # (just so you know its working)
-        msg.linear.x = -1 * msg.linear.x
+        if self.turtle_can_turn_ == 0:
+            msg.angular.z = 0.0
+        else:
+            self.get_logger().info('The turtle can still turn and move.')
+        if self.turtle_can_move_ == 0:
+            msg.linear.x = 0.0
+            msg.linear.y = 0.0
+        else:
+            msg.linear.x = -1 * msg.linear.x
+            self.get_logger().info('The turtle can still move.')
         self.get_logger().info(f'msg.linear.x: {msg.linear.x}')
         self.publisher.publish(msg)
         
     def controller_callback(self, msg):
+        if msg.data == 'start':
+            self.turtle_can_turn_ = 0
+            self.get_logger().warn('The turtle cannot turn anymore!')
+        if msg.data == 'stop':
+            self.turtle_can_move_ = 0
+            self.get_logger().warn('The turtle cannot move (or turn) anymore!')
         self.get_logger().warn(f'The controller says I should {msg.data} the turtle ...')
 
 
