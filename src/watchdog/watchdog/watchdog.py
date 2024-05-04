ed88ce34cc78d31a0ff35e6efd279ee0cc47501f
theodor-strauch <theodor.strauch@st.ovgu.de>
2024-05-04 19:37:27 +0200

    Assignment 1

--- src/watchdog/watchdog/watchdog.py
+++ src/watchdog/watchdog/watchdog.py
@@ -2,43 +2,35 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
 from std_msgs.msg import String
 
 class WatchdogNode(Node):
 
     def __init__(self):
         super().__init__('watchdog')
-        self.turtle_can_turn_ = 1
-        self.turtle_can_move_ = 1
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.create_subscription(Twist, 'input_cmd', self.cmd_callback, 10)
         self.create_subscription(String, 'controller_cmd', self.controller_callback, 10)
         self.get_logger().info('Watchdog node started')
+        self.controller_message=""
 
-    def cmd_callback(self, msg: Twist):
+    def cmd_callback(self, msg):
+        zero_vector= Vector3(x=0.0,y=0.0,z=0.0)
         # this makes the turle go backwards
         # (just so you know its working)
-        if self.turtle_can_turn_ == 0:
-            msg.angular.z = 0.0
-        else:
-            self.get_logger().info('The turtle can still turn and move.')
-        if self.turtle_can_move_ == 0:
-            msg.linear.x = 0.0
-            msg.linear.y = 0.0
-        else:
-            msg.linear.x = -1 * msg.linear.x
-            self.get_logger().info('The turtle can still move.')
+        if(self.controller_message=="start"):
+            msg.angular=zero_vector
+        elif(self.controller_message=="stop"):
+            msg.angular=zero_vector
+            msg.linear=zero_vector
+        
         self.get_logger().info(f'msg.linear.x: {msg.linear.x}')
         self.publisher.publish(msg)
         
     def controller_callback(self, msg):
-        if msg.data == 'start':
-            self.turtle_can_turn_ = 0
-            self.get_logger().warn('The turtle cannot turn anymore!')
-        if msg.data == 'stop':
-            self.turtle_can_move_ = 0
-            self.get_logger().warn('The turtle cannot move (or turn) anymore!')
         self.get_logger().warn(f'The controller says I should {msg.data} the turtle ...')
+        self.controller_message=msg.data
 
 
 
