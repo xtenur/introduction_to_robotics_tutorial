9cce57983db81e806ddb4acb0779f4988c554325
rpossardt <rune.possardt@st.ovgu.de>
2024-05-09 17:39:50 +0200

    feat: add 2 arbitrary modes

--- src/reactive_behaviour/reactive_behaviour/controller.py
+++ src/reactive_behaviour/reactive_behaviour/controller.py
@@ -9,19 +9,33 @@ import random
 
 class VelocityController(DrivingSwarmNode):
 
-    # use with ros2 launch reactive_behaviour robot.launch.py use_rviz:=false
+    # ros2 launch reactive_behaviour robot.launch.py use_rviz:=false
+    # ... (with arb mode 1 & 2 off)
+    # ... (with arb mode 1 on) -> sometimes off-balance
+    # ... (with arb mode 2 on)
+    #
+    # ros2 launch reactive_behaviour swarmlab.launch.py use_rviz:=false
+    # ... (with arb mode 1 & 2 off)
+    # ... (with arb mode 1 on) -> falls in narrow part
+    # ... (with arb mode 2 on)
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
 
         self.turn_around_narrow_corner: bool = False
         self.turn_bc_front_blocked: bool = False
-        self.randy: int = 1
+        self.random_pos_neg_one: int = 1
+        self.random_turn_vel: float = 0.0
+        # only one arb mode can be active
+        self.arb_mode_1: bool = False
+        self.arb_mode_2: bool = False
 
         self.ranges = []
         self.east = 0
         self.northeast = 0
+        self.no_no_ea = 0
         self.north = 0
+        self.no_no_we = 0
         self.northwest = 0
         self.west = 0
 
@@ -36,17 +50,31 @@ class VelocityController(DrivingSwarmNode):
         threshold_vel_x = 0.12
         turn_vel = 0.5
 
+        # x = np.min([self.north, self.northeast, self.northwest, self.no_no_ea, self.no_no_we]) - 0.3
         x = self.north - 0.3
         x = x if x < threshold_vel_x else threshold_vel_x
         x = x if x >= 0 else 0.0
         # negative velocity means going in direction of wheel-less side of robot
         msg.linear.x = -x
 
-        if self.west > 2.0:
-            msg.angular.z = random.randrange(10) * 0.1
+        # turn left or right arbitrarily
+        if self.arb_mode_1:
+            if random.randint(1, 5) == 3:
+                if self.northwest > 3.0:
+                    self.random_turn_vel = random.randrange(10) * 0.1
+                elif self.northeast > 3.0:
+                    self.random_turn_vel = -random.randrange(10) * 0.1
+                else:
+                    self.random_turn_vel = 0.0
+            msg.angular.z = self.random_turn_vel
+        elif self.arb_mode_2:
+            if self.northwest > 2.0 and self.no_no_we > 3.0:
+                msg.angular.z = random.randrange(10) * 0.1
+            elif self.northeast > 2.0 and self.no_no_ea > 3.0:
+                msg.angular.z = -random.randrange(10) * 0.1
 
         # set state for getting out of narrow corners
-        if self.north < 1.0 and self.northeast < 0.2 and self.northwest < 0.2:
+        if self.north < 0.5 and self.northeast < 0.2 and self.northwest < 0.2:
             self.turn_around_narrow_corner = True
         else:
             self.turn_around_narrow_corner = False
@@ -55,21 +83,23 @@ class VelocityController(DrivingSwarmNode):
             msg.angular.z = turn_vel
             msg.linear.x = 0.0
         else:
-            # as long as robot is walking straight, randy gets assigned new values
-            # if robot is in state turn_bc_front_blocked, it uses randy with an unchanging value
+            # as long as robot is walking straight, random_pos_neg_one gets assigned new values
+            # if robot is in state turn_bc_front_blocked, it uses random_pos_neg_one with an unchanging value
             if not self.turn_bc_front_blocked:
-                self.randy = -1 if random.randint(0, 1) == 0 else 1
+                self.random_pos_neg_one = -1 if random.randint(0, 1) == 0 else 1
 
             if self.north < 0.5:
                 self.turn_bc_front_blocked = True
-                # positive angular.z means turning left
-                msg.angular.z = self.randy * turn_vel
-            elif self.northwest < 0.5:
+                # positive angular.z means turning left; it is randomized with +1 or -1 here (turning left or right is arbitrary)
+                msg.angular.z = self.random_pos_neg_one * turn_vel
+            elif self.northwest < 0.5 or self.no_no_we < 0.5:
                 self.turn_bc_front_blocked = False
                 msg.angular.z = -turn_vel
-            elif self.northeast < 0.5:
+                # msg.angular.x *= 0.5
+            elif self.northeast < 0.5 or self.no_no_ea < 0.5:
                 self.turn_bc_front_blocked = False
                 msg.angular.z = turn_vel
+                # msg.angular.x *= 0.5
 
         self.publisher.publish(msg)
 
@@ -83,9 +113,8 @@ class VelocityController(DrivingSwarmNode):
         self.east = msg.ranges[(forward - 90) % 360]
         self.northwest = msg.ranges[(forward + 45) % 360]
         self.northeast = msg.ranges[(forward - 45) % 360]
-
-    def find_highest_range(self):
-        return np.max(self.ranges)
+        self.no_no_we = msg.ranges[(forward + 22) % 360]
+        self.no_no_ea = msg.ranges[(forward - 22) % 360]
 
 
 def main():
